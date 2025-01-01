929e297e82a6a8ca084b7a991df7ec7f60b62176
VictorDev <ememevictor08@gmail.com>
2025-01-02 00:31:16 +0100

    feat: ProductService implements the logic now

--- src/main/java/com/sb_demo_app/sbdemoapp/controller/ProductController.java
+++ src/main/java/com/sb_demo_app/sbdemoapp/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.sb_demo_app.sbdemoapp.controller;
 
 import com.sb_demo_app.sbdemoapp.model.Product;
+import com.sb_demo_app.sbdemoapp.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,20 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
 
-    // Ensure proper types in the constructor
-    public List<Product> products = Arrays.asList(
-            new Product(1L, "Samsung", 100.50, "Best Smart Phone"), // Long, String, Double, String
-            new Product(2L, "Redmi", 800.00, "Almost like iOS"),
-            new Product(3L, "Huawei", 500.00, "Underrated actually"),
-            new Product(4L, "Pixel", 600.00, "Best camera Smart Phones")
-    );
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/all")
-    public List<Product> getProducts() {
-        return products;
+    public List<Product> getAllProducts() {
+        return productService.getProducts();
     }
 }
--- src/main/java/com/sb_demo_app/sbdemoapp/service/ProductService.java
+++ src/main/java/com/sb_demo_app/sbdemoapp/service/ProductService.java
@@ -1,4 +1,22 @@
 package com.sb_demo_app.sbdemoapp.service;
 
+import com.sb_demo_app.sbdemoapp.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final List<Product> products = Arrays.asList(
+            new Product(1L, "Samsung", 100.50, "Best Smart Phone"), // Long, String, Double, String
+            new Product(2L, "Redmi", 800.00, "Almost like iOS"),
+            new Product(3L, "Huawei", 500.00, "Underrated actually"),
+            new Product(4L, "Pixel", 600.00, "Best camera Smart Phones")
+    );
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
